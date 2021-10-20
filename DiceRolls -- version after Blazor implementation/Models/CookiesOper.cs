28a8e3db69fbb4ea55903eb24075c699a1550ba3
PiotrKruk99 <86235717+PiotrKruk99@users.noreply.github.com>
2021-10-20 11:31:20 +0200

    Add files via upload

---
+++ DiceRolls -- version after Blazor implementation/Models/CookiesOper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace DiceRolls.Models
+{
+    public class CookiesOper
+    {
+        public static void Login(string login, int sessionId, HttpResponse response)
+        {
+            CookieOptions opts = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(14)
+            };
+
+            response.Cookies.Append(sessionId.ToString(), login, opts);
+
+            return;
+        }
+
+        public static string IsLogged(int sessionId, HttpRequest request)
+        {
+            string res = request.Cookies[sessionId.ToString()];
+
+            if (res == null)
+                return string.Empty;
+            else
+                return res;
+        }
+
+        public static void LastThrow(int diceCount, int diceType, HttpResponse response)
+        {
+            CookieOptions opts = new CookieOptions
+            {
+                Expires = DateTime.Now.AddMinutes(2)
+            };
+
+            response.Cookies.Append("diceCount", diceCount.ToString(), opts);
+            response.Cookies.Append("diceType", diceType.ToString(), opts);
+
+            return;
+        }
+
+        public static int GetCount(HttpRequest request)
+        {
+            string res = request.Cookies["diceCount"];
+
+            if (res == null)
+                return 1;
+            else
+                try { return Convert.ToInt32(res); }
+                catch (Exception) { return 1; }
+        }
+
+        public static int GetType(HttpRequest request)
+        {
+            string res = request.Cookies["diceType"];
+
+            if (res == null)
+                return 2;
+            else
+                try { return Convert.ToInt32(res); }
+                catch (Exception) { return 2; }
+        }
+    }
+}
---
+++ DiceRolls -- version after Blazor implementation/Models/UpdateMdl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRolls.Models
+{
+    public class UpdateMdl
+    {
+        public int Id { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
