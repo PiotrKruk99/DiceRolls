a587b11a55cb6556c451b6f06624d50b24067e8c
Piotr Kruk <biuro@liberezo.pl>
2025-08-09 15:34:14 +0200

    enum for dice types

---
+++ FullBlazorVersion/Enums/DiceTypeEnum.cs
@@ -0,0 +1,14 @@
+namespace FullBlazorVersion.Enums;
+
+public enum DiceTypeEnum
+{
+    K2 = 2,
+    K4 = 4,
+    K6 = 6,
+    K8 = 8,
+    K10 = 10,
+    K12 = 12,
+    K16 = 16,
+    K20 = 20,
+    K100 = 100
+}
