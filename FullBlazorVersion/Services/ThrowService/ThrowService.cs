7419ea37bf83b4f72cdcfa5f27c68d3802085338
Piotr Kruk <biuro@liberezo.pl>
2025-08-28 17:58:40 +0200

    added errors handling for throws

--- FullBlazorVersion/Services/ThrowService/ThrowService.cs
+++ FullBlazorVersion/Services/ThrowService/ThrowService.cs
@@ -2,6 +2,7 @@ using DiceRolls.Dtos;
 using DiceRolls.Repositories.ThrowRepository;
 using DiceRolls.Services.Randomize;
 using DiceRolls.Models;
+using DiceRolls.Services.ErrorMessage;
 
 namespace DiceRolls.Services.ThrowService;
 
@@ -9,45 +10,64 @@ public class ThrowService : IThrowService
 {
     private readonly IRandomizeService _randomizeService;
     private readonly IThrowRepository _throwRepository;
+    private readonly IErrorMessageService _errorMessageService;
 
-    public ThrowService(IRandomizeService randomizeService, IThrowRepository throwRepository)
+    public ThrowService(IRandomizeService randomizeService,
+        IThrowRepository throwRepository,
+        IErrorMessageService errorMessageService)
     {
         _randomizeService = randomizeService;
         _throwRepository = throwRepository;
+        _errorMessageService = errorMessageService;
     }
 
     public async Task ThrowDice(ThrowDiceInputDto input)
     {
-        var roll = _randomizeService.GetRandomDice(input.Count, input.DiceType);
-
-        Throw throwDice = new Throw()
+        try
         {
-            SessionId = input.SessionId,
-            Login = input.Login,
-            Date = DateTime.UtcNow,
-            DiceType = input.DiceType,
-            DiceValues = string.Join(',', roll)
-        };
+            var roll = _randomizeService.GetRandomDice(input.Count, input.DiceType);
 
-        await _throwRepository.AddThrow(throwDice);
+            Throw throwDice = new Throw()
+            {
+                SessionId = input.SessionId,
+                Login = input.Login,
+                Date = DateTime.UtcNow,
+                DiceType = input.DiceType,
+                DiceValues = string.Join(',', roll)
+            };
+
+            await _throwRepository.AddThrow(throwDice);
+        }
+        catch (Exception)
+        {
+            _errorMessageService.SetSessionErrorMessage("Nie udało się zapisać rzutu do bazy.", input.SessionId);
+        }
     }
-    
+
     public async Task<IEnumerable<ThrowDiceOutputDto>> GetThrows(string sessionId)
     {
-        var throws = await _throwRepository.GetThrows(sessionId);
         var result = new List<ThrowDiceOutputDto>();
 
-        foreach (var elem in throws.OrderByDescending(x => x.Date))
+        try
         {
-            result.Add(new ThrowDiceOutputDto()
+            var throws = await _throwRepository.GetThrows(sessionId);
+
+            foreach (var elem in throws.OrderByDescending(x => x.Date))
             {
-                Login = elem.Login ?? "",
-                Time = elem.Date.ToLocalTime().ToString("HH:mm:ss"),
-                DiceType = elem.DiceType.ToString().ToLower(),
-                DiceValues = elem.DiceValues.Split(',').Select(x => Convert.ToInt32(x))
-            });
+                result.Add(new ThrowDiceOutputDto()
+                {
+                    Login = elem.Login ?? "",
+                    Time = elem.Date.ToLocalTime().ToString("HH:mm:ss"),
+                    DiceType = elem.DiceType.ToString().ToLower(),
+                    DiceValues = elem.DiceValues.Split(',').Select(x => Convert.ToInt32(x))
+                });
+            }
         }
-        
+        catch (Exception)
+        {
+            _errorMessageService.SetSessionErrorMessage("Nie udało się pobrać listy rzutów z bazy.", sessionId);
+        }
+
         return result;
     }
 }
